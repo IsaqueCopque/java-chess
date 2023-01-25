2b0bb84a67bf917df15088c2e8056483a2cff6d8
IsaqueCopque <71469931+IsaqueCopque@users.noreply.github.com>
2023-01-25 11:29:19 -0300

    adiciona posicao xadrez e excecao

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -25,6 +26,12 @@ public class ChessMatch {
 	}
 	
 	public void initialSetup() {
-		//board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
+		placeNewPiece('b',6, new Rook(board,Color.WHITE));
+		placeNewPiece('e',1, new King(board,Color.BLACK));
+	}
+	
+	//Coloca peça na partida de acordo com posição do xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 }
